edd5fc93c26b2ea13d3a21d8caa9448358616f9c
hemantjhil <32435133+hemantjhil@users.noreply.github.com>
2019-05-20 16:38:05 +0530

    Add files via upload

---
+++ textwrap.py
@@ -0,0 +1,29 @@
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
+
+
+>>> import textwrap
+>>> string = "This is a very very very very very long string."
+>>> print textwrap.wrap(string,8)
+['This is', 'a very', 'very', 'very', 'very', 'very', 'long', 'string.']
+
+>>> import textwrap
+>>> string = "This is a very very very very very long string."
+>>> print textwrap.fill(string,8)
+This is
+a very
+very
+very
+very
+very
+long
+string.
